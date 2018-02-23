9c2ab570a942af52f75f0a1b45e11ddae16f78a0
Tomasz Maćkowiak <kurazu@kurazu.net>
2018-02-23 23:58:16 +0100

    batch bottleneck

--- train.py
+++ train.py
@@ -5,8 +5,9 @@ from glob import glob
 import h5py
 import functools
 import numpy as np
-import itertools
 from tqdm import tqdm
+from multiprocessing import Pool
+import math
 
 HERE = os.path.dirname(__file__)
 DATA_DIR = os.path.join(HERE, 'data')
@@ -27,11 +28,11 @@ def to_list(func):
     return to_list_wrapper
 
 
-def grouper(iterable, n, fillvalue=None):
+def grouper(iterable, n):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
-    return itertools.izip(*args, fillvalue=fillvalue)
+    return zip(*args)
 
 
 @to_list
@@ -53,23 +54,54 @@ CLASSES = [
     'nevus',
     'seborrheic_keratosis'
 ]
+
+
+def get_mask(n, one_idx):
+    result = np.zeros(n)
+    result[one_idx] = 1.0
+    return result
+
+
 CLASS_MAPPING = {
-    name: i
+    name: get_mask(len(CLASSES), i)
     for i, name in enumerate(CLASSES)
 }
 
+
+def load_img_array(path):
+    img = image.load_img(path, target_size=IMAGE_INPUT_SIZE)
+    img_arr = image.img_to_array(img, data_format='channels_last')
+    return img_arr
+
+
 def store_bottleneck_features(f, pretrained_model, name, dataset):
+    batch_size = 32
+    workers = 16
     features_shape = (len(dataset), *pretrained_model.output_shape[1:])
-    features_dset = f.create_dataset(f'{name}_features', features_shape, dtype='f')
-    labels_dset = f.create_dataset(f'{name}_labels', (len(dataset), len(CLASSES)), dtype='f')
-    for sample_idx, (path, class_name) in enumerate(tqdm(dataset)):
-        class_idx = CLASS_MAPPING[class_name]
-        labels_dset[sample_idx, class_idx] = 1.0
-        img = image.load_img(path, target_size=IMAGE_INPUT_SIZE)
-        img_arr = image.img_to_array(img, data_format='channels_last')
-        img_tensor = img_arr.reshape((1, *img_arr.shape))
-        predictions = pretrained_model.predict(img_tensor)
-        features_dset[sample_idx] = predictions[0]
+    features_dset = f.create_dataset(
+        f'{name}_features', features_shape, dtype='f'
+    )
+    labels_dset = f.create_dataset(
+        f'{name}_labels', (len(dataset), len(CLASSES)), dtype='f'
+    )
+    batches = grouper(dataset, batch_size)
+    total_batches = math.ceil(len(dataset) / batch_size)
+    with Pool(workers) as pool:
+        for batch_idx, batch in enumerate(tqdm(batches, total=total_batches)):
+            start_idx = batch_idx * batch_size
+            end_idx = start_idx + len(batch)
+
+            filenames = [path for path, _ in batch]
+            img_arrays = pool.map(load_img_array, filenames)
+            img_tensors = np.stack(img_arrays)
+            predictions = pretrained_model.predict(img_tensors)
+            features_dset[start_idx:end_idx] = predictions
+
+            class_names = (class_name for _, class_name in batch)
+            labels = np.stack([
+                CLASS_MAPPING[class_name] for class_name in class_names
+            ])
+            labels_dset[start_idx:end_idx] = labels
 
 
 def main():
